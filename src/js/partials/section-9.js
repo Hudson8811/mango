6f5f7ef94b50e9febbc31787e68dfa937e1fe512
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-23 19:24:38 +0600

    b8

--- src/js/partials/section-9.js
+++ src/js/partials/section-9.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.portfolio .swiper-container', {
+var swiperProtfolio = new Swiper('.portfolio .swiper-container', {
     effect: 'coverflow',
     centeredSlides: true,
     slidesPerView: 'auto',
@@ -18,4 +18,16 @@ var swiper = new Swiper('.portfolio .swiper-container', {
         nextEl: '.portfolio .swiper-button-next',
         prevEl: '.portfolio .swiper-button-prev',
     },
+    on:{
+        init: function () {
+            var slide = $('.portfolio .swiper-slide-active');
+            $('.portfolio__title').html(slide.find('.hidden-content__title').html());
+            $('.portfolio__text').html(slide.find('.hidden-content__text').html());
+        },
+        transitionEnd: function () {
+            var slide = $('.portfolio .swiper-slide-active');
+            $('.portfolio__title').html(slide.find('.hidden-content__title').html());
+            $('.portfolio__text').html(slide.find('.hidden-content__text').html());
+        },
+    }
 });

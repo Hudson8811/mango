c3f160dbc24ced3f43c43b0a339083f559f033fa
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-24 16:39:19 +0600

    modals

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $('.footer').fancybox({
-        src  : '#callback-form',
+    $('.fancybox-btn').fancybox({
         modal : true,
+    });
+
+    $('.modal-block__close').click(function () {
+        $.fancybox.close();
+    });
+
+    $('.modal-block__button_close').click(function () {
+        $.fancybox.close();
+    });
+
+    $('.modal-block__form').submit(function () {
+       event.preventDefault();
+        $(this).siblings('.modal-block__subtitle').fadeOut(300);
+       $(this).fadeOut(300,function () {
+           $(this).siblings('.modal-block__button_close').css("display", "flex");
+           $(this).siblings('.modal-block__subtitle_success').css("display", "block");
+       });
     });
 });

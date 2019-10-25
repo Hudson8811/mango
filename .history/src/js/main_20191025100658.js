603431ada74c91ef59b5333183965953d39a5a1a
db21 <you@example.com>
2019-10-25 11:31:16 +0300

    123

---
+++ .history/src/js/main_20191025100658.js
@@ -0,0 +1,14 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/swiper.min.js
+//= libs/jquery.fancybox.min.js
+
+/* my scripts */
+
+//= partials/main.js
+//= partials/header.js
+//= partials/section-9
+//= partials/section-11
+//= partials/section-5
+//= partials/section-6
---
+++ .history/src/js/partials/main_20191025105158.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('.fancybox-btn').fancybox({
+        clickOutside: "close",
+        smallBtn: false,
+        buttons: false,
+        touch: false
+    });
+
+    $('.modal-block__close').click(function () {
+        $.fancybox.close();
+    });
+
+    $('.modal-block__button_close').click(function () {
+        $.fancybox.close();
+    });
+
+    $('.modal-block__form').submit(function () {
+        event.preventDefault();
+        $(this).siblings('.modal-block__subtitle').fadeOut(300);
+        $(this).fadeOut(300, function () {
+            $(this).siblings('.modal-block__button_close').css("display", "flex");
+            $(this).siblings('.modal-block__subtitle_success').css("display", "block");
+        });
+    });
+});
+
+var d = new Date();
+
+var monthJs = d.getMonth() + 1;
+var dayJs = d.getDate();
+if (dayJs > 30) monthJs++;
+if (monthJs > 12) monthJs = 1;
+if (monthJs == 4) { dayJs = "28" } else { dayJs = "30" }
+var output = dayJs + '.' + (monthJs < 10 ? '0' : '') + monthJs;
+$('#dateJs').html(output);
+
+
+function offWow() {
+    if ($(window).width() <= 991) {
+        $(".wow").removeClass("wow");
+    }
+}
+$(document).ready(function () {
+    offWow();
+}

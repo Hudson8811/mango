646c3173947b191c5e5ece5d6a7fc53ee6819659
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-31 16:59:40 +0600

    animate

--- src/js/partials/section-11.js
+++ src/js/partials/section-11.js
@@ -3,4 +3,45 @@ $(document).ready(function() {
         $('.reviews__item').slideDown();
         $(this).fadeOut();
     });
+
+
+    var collapsedHeightName = $('.reviews__name').css('maxHeight');
+    var collapsedHeightRole = $('.reviews__role').css('maxHeight');
+    var collapsedHeightText = $('.reviews__text').css('maxHeight');
+
+
+    $('.reviews__name').each(function(){
+        var $this = $(this);
+        $this.data('natural', $this[0].scrollHeight);
+    });
+    $('.reviews__role').each(function(){
+        var $this = $(this);
+        $this.data('natural', $this[0].scrollHeight);
+    });
+    $('.reviews__text').each(function(){
+        var $this = $(this);
+        $this.data('natural', $this[0].scrollHeight);
+    });
+
+
+    $('.reviews__item').hover(function(){
+        var $rName = $(this).find('.reviews__name').eq(0),
+            $rRole = $(this).find('.reviews__role').eq(0),
+            $rText = $(this).find('.reviews__text').eq(0),
+            duration = 250;
+
+        $rName.animate({'maxHeight': $rName.data('natural')}, duration);
+        $rRole.animate({'maxHeight': $rRole.data('natural')}, duration);
+        $rText.animate({'maxHeight': $rText.data('natural')}, duration);
+        $(this).animate({'zIndex': 1}, 0);
+    },function () {
+        var $rName = $(this).find('.reviews__name').eq(0),
+            $rRole = $(this).find('.reviews__role').eq(0),
+            $rText = $(this).find('.reviews__text').eq(0),
+            duration = 250;
+        $rName.animate({'maxHeight': collapsedHeightName}, duration);
+        $rRole.animate({'maxHeight': collapsedHeightRole}, duration);
+        $rText.animate({'maxHeight': collapsedHeightText}, duration);
+        $(this).animate({'zIndex': 0}, duration);
+    });
 });
